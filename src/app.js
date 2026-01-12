fee2b9e7e36b1d7e5eec55bf014bc7970dfbdf18
Md Saddam <mdsonu7860@outlook.com>
2026-01-12 15:56:39 +0530

    Update health check response message in app.js

--- src/app.js
+++ src/app.js
@@ -6,7 +6,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/health", (req, res) => {
-  res.status(200).send("OK");
+  res
+    .status(200)
+    .send(`Health is good, Server is running on port ${process.env.PORT}`);
 });
 
 export { app };
