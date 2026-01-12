c4fd27bcab0b0315d6534be2637122d3dcf23501
Md Saddam <mdsonu7860@outlook.com>
2026-01-12 16:36:41 +0530

    Refactor logging and remove dotenv configuration from gongAxios.config.js

--- src/configs/gongAxios.config.js
+++ src/configs/gongAxios.config.js
@@ -1,5 +1,3 @@
-import dotenv from "dotenv";
-dotenv.config();
 import axios from "axios";
 import fs from "fs";
 import path from "path";
--- src/intermediaToGong.js
+++ src/intermediaToGong.js
@@ -4,7 +4,7 @@ import { logger } from "./index.js";
 import { startIntermediaPolling } from "./schedulers/intermedia.poller.js";
 
 const PORT = process.env.PORT || 5000;
-logger.info("PORT from env:", process.env.PORT);
+logger.info(`PORT from env: ${process.env.PORT} `);
 
 app.listen(PORT, async () => {
   logger.info(`Server is running on port ${PORT}`);
