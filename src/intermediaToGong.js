7e7a16ba0bd0dd3cd2b904f3b03c851f6048d298
Md Saddam <mdsonu7860@outlook.com>
2026-01-12 16:04:54 +0530

    Refactor dotenv import to use shorthand syntax

--- src/intermediaToGong.js
+++ src/intermediaToGong.js
@@ -1,11 +1,10 @@
-// import "dotenv/config";
-import dotenv from "dotenv";
-dotenv.config();
+import "dotenv/config";
 import { app } from "./app.js";
 import { logger } from "./index.js";
 import { startIntermediaPolling } from "./schedulers/intermedia.poller.js";
 
 const PORT = process.env.PORT || 5000;
+logger.info("PORT from env:", process.env.PORT);
 
 app.listen(PORT, async () => {
   logger.info(`Server is running on port ${PORT}`);
