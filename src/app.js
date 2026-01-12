08ed396b58d37788f4c4992b7ca9f70e40255c13
Md Saddam <mdsonu7860@outlook.com>
2026-01-12 15:55:39 +0530

    Add health check endpoint to the application

--- src/app.js
+++ src/app.js
@@ -5,4 +5,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
 export { app };
