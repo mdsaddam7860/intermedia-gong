d4e44b894a8a2004e2d1cbcbe4b26f87a1328048
Md Saddam <mdsonu7860@outlook.com>
2026-01-14 14:29:07 +0530

    Refactor historic records sync controller and update dotenv configuration in intermediaToGong.js

--- src/intermediaToGong.js
+++ src/intermediaToGong.js
@@ -1,9 +1,18 @@
 import "dotenv/config";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({
+  path: path.resolve(process.cwd(), "../.env"),
+});
+
 import { app } from "./app.js";
 import { logger } from "./index.js";
 import { startIntermediaPolling } from "./schedulers/intermedia.poller.js";
 
 const PORT = process.env.PORT || 5000;
+
+logger.info(`CWD: ${process.cwd()}`);
 logger.info(`PORT from env: ${process.env.PORT} `);
 
 app.listen(PORT, async () => {
